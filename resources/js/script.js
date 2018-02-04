602b6cc31cd4eb03a0609a21e6448f9a280e3c24
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-02-04 19:04:36 +0200

    Added nav scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	//fixed nav
 	$(".js--features").waypoint(function(direction) {
 		if(direction == "down") {
 			$("nav").addClass("hero__nav--fixed");
@@ -10,5 +11,39 @@ $(document).ready(function() {
 	}, { 
 		offset: "65px;" 
 	});
-	
+
+	//scroll on buttons
+	$(".js--scroll-to-plans").click(function(){
+		$("html, body").animate({scrollTop: $(".js--plans").offset().top}, 1000);
+	})
+	$(".js--scroll-to-start").click(function(){
+		$("html, body").animate({scrollTop: $(".js--features").offset().top}, 1000);
+	})
+
+	//nav scroll
+	$('a[href*="#"]')
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.click(function(event) {
+			//on-page links
+			if (
+				location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+				&& 
+				location.hostname == this.hostname
+			) {
+				// Figure out element to scroll to
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				// Does a scroll target exist?
+				if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+				}
+			}
+		});
+
 });
