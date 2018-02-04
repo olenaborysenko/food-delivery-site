d1950620e37b3921b6602284ade58e6bde6911ac
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-02-04 19:45:12 +0200

    Completed animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -42,8 +42,33 @@ $(document).ready(function() {
 					scrollTop: target.offset().top
 				}, 1000);
 				return false;
-				}
 			}
-		});
+		}
+	});
+
+	//animations on scroll
+	$(".js--wp-1").waypoint(function(direction) {
+		$(".js--wp-1").addClass("animated fadeIn");
+	}, {
+		offset: "50%"
+	});
+
+	$(".js--wp-2").waypoint(function(direction) {
+		$(".js--wp-2").addClass("animated shake");
+	}, {
+		offset: "50%"
+	});
+
+	$(".js--wp-3").waypoint(function(direction) {
+		$(".js--wp-3").addClass("animated fadeIn");
+	}, {
+		offset: "50%"
+	});
+
+	$(".js--wp-4").waypoint(function(direction) {
+		$(".js--wp-4").addClass("animated pulse");
+	}, {
+		offset: "50%"
+	});
 
 });
